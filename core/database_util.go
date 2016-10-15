0bdd06f222d63171c44991b7fc6339f59600eb08
martymcfly2015 <martymcfly2015@protonmail.com>
2016-10-15 16:40:17 +0200

    HardFork difficulty

--- core/database_util.go
+++ core/database_util.go
@@ -58,6 +58,15 @@ var (
 // the difficulty that a new block b should have when created at time
 // given the parent block's time and difficulty.
 func CalcDifficulty(time, parentTime uint64, parentNumber, parentDiff *big.Int) *big.Int {
+	//HardFork
+	if parentNumber.Cmp(params.HardFork2) < 0 {
+		params.DurationLimit = big.NewInt(13)		
+	} else if  (parentNumber.Cmp(params.HardFork2) > -1) &&  (parentNumber.Cmp(params.HardFork3) <0) {
+		params.DurationLimit =  big.NewInt(60)
+	} else {
+		params.DurationLimit =  big.NewInt(13)
+	}
+		
 	diff := new(big.Int)
 	adjust := new(big.Int).Div(parentDiff, params.DifficultyBoundDivisor)
 	bigTime := new(big.Int)
