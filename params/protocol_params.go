39b6f39f2b57bc19af73d3febe81cbfb3ee5eb4e
martymcfly2015 <martymcfly2015@protonmail.com>
2016-10-15 16:33:04 +0200

    soil protocol params

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -29,7 +29,11 @@ var (
 	CallNewAccountGas      = big.NewInt(25000)  // Paid for CALL when the destination address didn't exist prior.
 	TxGas                  = big.NewInt(21000)  // Per transaction. NOTE: Not payable on data of calls between transactions.
 	TxDataZeroGas          = big.NewInt(4)      // Per byte of data attached to a transaction that equals zero. NOTE: Not payable on data of calls between transactions.
-	DifficultyBoundDivisor = big.NewInt(2048)   // The bound divisor of the difficulty, used in the update calculations.
+	//DifficultyBoundDivisor = big.NewInt(2048)   // The bound divisor of the difficulty, used in the update calculations.
+	DifficultyBoundDivisor = big.NewInt(1024)
+	HardFork1              = big.NewInt(263708) // Hard fork block reward 8 to 4 SOIL
+	HardFork2              = big.NewInt(320000) // Hard fork block time to 60 seconds
+	HardFork3              = big.NewInt(334000) // Hard fork block time to back to 15 seconds and block reward to 1 SOIL		
 	QuadCoeffDiv           = big.NewInt(512)    // Divisor for the quadratic particle of the memory cost equation.
 	GenesisDifficulty      = big.NewInt(131072) // Difficulty of the Genesis block.
 	DurationLimit          = big.NewInt(13)     // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
